aeafbe7c3ebde300632c5a845b8d429ed5928863
diego caviedes <diego.caviedes@cargologik.com>
2022-11-01 01:18:40 -0500

    iterando arreglo en una lista

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 function Home() {
   //let a = 1;
   const [data, setData] = useState(1);
+  const arrgeglo = [
+    {
+        id: 1,
+        nombre: 'Diego',
+    },
+    {   id: 2,
+        nombre: 'Cristian',
+    },
+]
 
   const handleClick = () => {
     //a = 10;null
@@ -13,7 +22,13 @@ function Home() {
             <div>Home</div>
             <span>{data}</span>
             <button onClick={handleClick}>Transformamos a</button>
+        <ul>
+            {arrgeglo.map(item => (
+                <li key={item.id}> {item.nombre}</li>)
+            )}
+        </ul>
         </div>
+
         /* <> tag vacio
             <div>Home</div>
             <span>{a}</span>
