64ca2ec9d44bb2b6ac759c55ba0114faccc20329
diego caviedes <diego.caviedes@cargologik.com>
2022-11-01 01:10:14 -0500

    Hook useState para manejar el estado de la variable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 
 function Home() {
-  let a = 1;
+  //let a = 1;
+  const [data, setData] = useState(1);
+
   const handleClick = () => {
-    a = 10;
+    //a = 10;null
+    setData(10);
   }
     return (
         <div>
             <div>Home</div>
-            <span>{a}</span>
+            <span>{data}</span>
             <button onClick={handleClick}>Transformamos a</button>
         </div>
         /* <> tag vacio
